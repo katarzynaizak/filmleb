f8d23d4cc08ca1bfd27a4c4f3afd99de09f3acf5
Katarzyna Izak <kasik83@gmail.com>
2018-11-20 19:42:11 +0100

    dodanie tablicy obiektow na start

--- js-apps-starter/js/main.js
+++ js-apps-starter/js/main.js
@@ -1,6 +1,34 @@
 let counter = 1;
 
-$('#add').on('click', function() {
+const films = [
+    {
+        name: 'W pustyni i w puszczy',
+        director: 'Władysław Ślesicki',
+        production: 1973
+    },
+    {
+        name: 'Krzyżacy',
+        director: 'Aleksander Ford',
+        production: 1960
+    }
+];
+
+for (let film of films) {
+    const newEl = $(`
+        <tr>
+            <th scope="row">${counter}</th>
+            <td>${film.name}</td>
+            <td>${film.director}</td>
+            <td>${film.production}</td>
+        </tr>
+    `);
+
+    $('tbody').append(newEl);
+
+    counter = counter + 1;
+}
+
+$('#add').on('click', function() { //submit dla entera
     const filmName = $('#filmName').val();
     const filmAuthor = $('#filmAuthor').val();
     const filmYear = $('#filmYear').val();
