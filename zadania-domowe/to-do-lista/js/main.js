626320bf7b5695d598691bd9383372c1b3e41c84
Katarzyna Izak <kasik83@gmail.com>
2018-11-20 19:55:44 +0100

    dodanie tablicy obiektow na start

--- zadania-domowe/to-do-lista/js/main.js
+++ zadania-domowe/to-do-lista/js/main.js
@@ -1,3 +1,23 @@
+const listOfToDo = [
+    {
+        task: 'Opłacić rachunki'
+    },
+    {
+        task: 'Spakować plecak'
+    },
+    {
+        task: 'Zrobić zakupy'
+    }
+];
+
+for (let item of listOfToDo) {
+    const newEl = $(`
+        <li>${item.task}</li>
+    `);
+
+    $('ul').append(newEl);
+};
+
 $('#add').on('click', function() {
     const request = $('#toDoInput').val();
 
