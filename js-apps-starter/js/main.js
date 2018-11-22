c50de131868371fb2d3cb19a32d54392494ff892
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2018-11-22 22:41:36 +0100

    dodane komentarze do kodu

--- js-apps-starter/js/main.js
+++ js-apps-starter/js/main.js
@@ -1,20 +1,38 @@
-let counter = 1;
+let counter = 1; // licznik pozycji na stronie
 
+// baza już dodanych filmów - obiekty w tablicy
 const films = [
     {
-        name: 'W pustyni i w puszczy',
-        director: 'Władysław Ślesicki',
-        production: 1973
+        name: 'Krokodyl Dundee',
+        director: 'Peter Faiman',
+        production: 1986
     },
     {
-        name: 'Krzyżacy',
-        director: 'Aleksander Ford',
-        production: 1960
+        name: 'Kołysanka',
+        director: 'Juliusz Machulski',
+        production: 2010
+    },
+    {
+        name: 'Zapach kobiety',
+        director: 'Martin Brest',
+        production: 1992
+    },
+    {
+        name: 'Zielona Mila',
+        director: 'Frank Darabont',
+        production: 1999
+    },
+    {
+        name: 'Adwokat diabła',
+        director: 'Taylor Hackford',
+        production: 1997
     }
 ];
 
+// pętla pokazująca filmy z istniejącej bazy (z tablicy)
 for (let film of films) {
-    const newEl = $(`
+    // tworzenie wiersza z danymi filmu (z bazy już dodanych filmów)
+    const showFilm = $(`
         <tr>
             <th scope="row">${counter}</th>
             <td>${film.name}</td>
@@ -23,17 +41,19 @@ for (let film of films) {
         </tr>
     `);
 
-    $('tbody').append(newEl);
+    $('tbody').append(showFilm); // wstawianie wiersza z filmem (pobranym z bazy) na stronę
 
     counter = counter + 1;
 }
 
 $('#add').on('click', function() { //submit dla entera
+    // pobieranie wartości z modala
     const filmName = $('#filmName').val();
     const filmAuthor = $('#filmAuthor').val();
     const filmYear = $('#filmYear').val();
 
-    const newEl = $(`
+    // tworzenie wiersza z danymi filmu (dodawanego za pomocą formularza)
+    const addFilm = $(`
         <tr>
             <th scope="row">${counter}</th>
             <td>${filmName}</td>
@@ -42,9 +62,9 @@ $('#add').on('click', function() { //submit dla entera
         </tr>
     `);
 
-    $('tbody').append(newEl);
+    $('tbody').append(addFilm); // wstawianie wiersza z nowym filmem na stronę
     $('#exampleModal').modal('hide'); // zamykanie okna modalu
-    $('form').trigger('reset'); // zresetowanie formularza
+    $('form').trigger('reset'); // zresetowanie pól formularza
 
     counter = counter + 1;
 })
