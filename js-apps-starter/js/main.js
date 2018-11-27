ea84b69f553cb63502c02fa1c95db21eacaa2781
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2018-11-27 19:33:59 +0100

    pobieranie i wyswietlanie danych z filmami z bazy Firebase

--- js-apps-starter/js/main.js
+++ js-apps-starter/js/main.js
@@ -1,50 +1,24 @@
 let counter = 1; // licznik pozycji na stronie
 
-// baza już dodanych filmów - obiekty w tablicy
-const films = [
-    {
-        name: 'Krokodyl Dundee',
-        director: 'Peter Faiman',
-        production: 1986
-    },
-    {
-        name: 'Kołysanka',
-        director: 'Juliusz Machulski',
-        production: 2010
-    },
-    {
-        name: 'Zapach kobiety',
-        director: 'Martin Brest',
-        production: 1992
-    },
-    {
-        name: 'Zielona Mila',
-        director: 'Frank Darabont',
-        production: 1999
-    },
-    {
-        name: 'Adwokat diabła',
-        director: 'Taylor Hackford',
-        production: 1997
-    }
-];
+firebase
+    .database()
+    .ref('/films')
+    .on('child_added', function(data) {
+        const filmFromDB = data.val();
 
-// pętla pokazująca filmy z istniejącej bazy (z tablicy)
-for (let film of films) {
-    // tworzenie wiersza z danymi filmu (z bazy już dodanych filmów)
-    const showFilm = $(`
-        <tr>
-            <th scope="row">${counter}</th>
-            <td>${film.name}</td>
-            <td>${film.director}</td>
-            <td>${film.production}</td>
-        </tr>
-    `);
+        const showFilm = $(`
+            <tr>
+                <th scope="row">${counter}</th>
+                <td>${filmFromDB.name}</td>
+                <td>${filmFromDB.director}</td>
+                <td>${filmFromDB.production}</td>
+            </tr>
+        `);
 
-    $('tbody').append(showFilm); // wstawianie wiersza z filmem (pobranym z bazy) na stronę
+        $('tbody').append(showFilm); // wstawianie wiersza z filmem (pobranym z bazy) na stronę
 
-    counter = counter + 1;
-}
+        counter = counter + 1;
+    });
 
 $('#add').on('click', function() { // wysyłanie danych z formularza
     // pobieranie wartości z modala
@@ -52,13 +26,13 @@ $('#add').on('click', function() { // wysyłanie danych z formularza
     const filmAuthor = $('#filmAuthor').val();
     const filmYear = $('#filmYear').val();
 
-    const film = {
+    const addFilm = {
         name: filmName,
         director: filmAuthor,
         production: filmYear
     }
 
-    firebase.database().ref('/films').push(film);
+    firebase.database().ref('/films').push(addFilm);
 
     $('#exampleModal').modal('hide'); // zamykanie okna modalu
     $('form').trigger('reset'); // zresetowanie pól formularza
