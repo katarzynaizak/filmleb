011a5cbc0b1bcf80f5f19332d3ae6213696eaa89
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2018-12-04 21:31:19 +0100

    dodanie edycji filmow

--- js-apps-starter/js/main.js
+++ js-apps-starter/js/main.js
@@ -14,8 +14,8 @@ firebase
                 <td>${filmFromDB.production}</td>
                 <td data-key="${data.key}">
                     <i class="fas fa-trash" id="delete"></i>
-                    <i class="fas fa-pencil-alt"></i>
-                    <i class="fas fa-check"></i>
+                    <i class="fas fa-pencil-alt" id="edit"></i>
+                    <i class="fas fa-check invisible" id="accept"></i>
                 </td>
             </tr>
         `);
@@ -37,9 +37,7 @@ $('#add').on('click', function() { // wysyłanie danych z formularza
         production: filmYear
     }
 
-    firebase.database()
-        .ref('/films')
-        .push(addFilm);
+    firebase.database().ref('/films').push(addFilm); // dodajemy do bazy
 
     $('#exampleModal').modal('hide'); // zamykanie okna modalu
     $('form').trigger('reset'); // zresetowanie pól formularza
@@ -48,6 +46,7 @@ $('#add').on('click', function() { // wysyłanie danych z formularza
 $('body').on('click', '#delete', function() { // "propagacja" dla elementów dynamicznych na stronie (ikonki pojawiają się na stronie dynamicznie, a nie statycznie)
     const self = $(this);
     const key = self.parent().data('key');
+
     firebase.database()
         .ref(`/films/${key}`)
         .remove()
@@ -55,4 +54,46 @@ $('body').on('click', '#delete', function() { // "propagacja" dla elementów dyn
             //window.location.reload(); // 1 sposób - odświeżam całą stronę
             self.parent().parent().remove() // 2 sposób - ręcznie usuwam z html'a
         })
+})
+
+$('body').on('click', '#edit', function() {
+    const self = $(this);
+
+    const currentName = self.parent().parent().children().eq(1); // grzebanie w dziadku :-)
+    const currentAuthor = currentName.next(); // metoda na brata :-D
+    const currentYear = currentAuthor.next();
+
+    currentName.attr('contenteditable', true);
+    currentAuthor.attr('contenteditable', true);
+    currentYear.attr('contenteditable', true);
+
+    self.addClass('invisible'); // ikonka do edycji znika
+    self.next().removeClass('invisible'); // ikonka do potwierdzenia edycji pojawia się
+})
+
+$('body').on('click', '#accept', function() {
+    const self = $(this);
+    const key = self.parent().data('key');
+
+    const currentName = self.parent().parent().children().eq(1); // grzebanie w dziadku :-)
+    const currentAuthor = currentName.next(); // metoda na brata :-D
+    const currentYear = currentAuthor.next();
+
+    currentName.attr('contenteditable', false);
+    currentAuthor.attr('contenteditable', false);
+    currentYear.attr('contenteditable', false);
+
+    const editedFilm = {
+        name: currentName.text(),
+        director: currentAuthor.text(),
+        production: currentYear.text()
+    }
+
+    firebase
+        .database()
+        .ref(`/films/${key}`)
+        .set(editedFilm); // edytujemy w bazie danych
+
+    self.addClass('invisible'); // ikonka do potwierdzenia edycji znika
+    self.prev().removeClass('invisible'); // ikonka do edycji pojawia się
 })
