e408167e75933a6bb7b00b55ef475e9a6f35010a
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-04 12:28:15 +0200

    change file structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,37 @@
+"use strict";
+
 let counter = 1; // licznik pozycji na stronie
 
 firebase
   .database()
   .ref('/films')
   .on('child_added', function(data) {
-      const filmFromDB = data.val();
+    const filmFromDB = data.val();
 
-      const showFilm = $(`
-          <tr>
-              <th scope="row">${counter}</th>
-              <td>${filmFromDB.name}</td>
-              <td>${filmFromDB.director}</td>
-              <td>${filmFromDB.production}</td>
-              <td>${filmFromDB.type}</td>
-              <td>${filmFromDB.kind}</td>
-              <td>${filmFromDB.carrier}</td>
-              <td data-key="${data.key}">
-                  <i class="fas fa-trash" id="delete"></i>
-                  <i class="fas fa-pencil-alt" id="edit"></i>
-                  <i class="fas fa-check invisible" id="accept"></i>
-              </td>
-          </tr>
-      `);
+    const showFilm = $(`
+      <tr>
+        <th scope="row">${counter}</th>
+        <td>${filmFromDB.name}</td>
+        <td>${filmFromDB.director}</td>
+        <td>${filmFromDB.production}</td>
+        <td>${filmFromDB.type}</td>
+        <td>${filmFromDB.kind}</td>
+        <td>${filmFromDB.carrier}</td>
+        <td data-key="${data.key}">
+          <i class="fas fa-trash" id="delete"></i>
+          <i class="fas fa-pencil-alt" id="edit"></i>
+          <i class="fas fa-check invisible" id="accept"></i>
+        </td>
+      </tr>
+    `);
 
-      $('tbody').append(showFilm); // wstawianie wiersza z filmem (pobranym z bazy) na stronę
+    $('tbody').append(showFilm); // wstawianie wiersza z filmem (pobranym z bazy) na stronę
 
-      counter = counter + 1;
+    counter = counter + 1;
   });
 
-$('#add').on('click', function() { // wysyłanie danych z formularza
+// wysyłanie danych z formularza
+$('#add').on('click', function() {
   // pobieranie wartości z modala
   const filmName = $('#filmName').val();
   const filmAuthor = $('#filmAuthor').val();
@@ -38,31 +41,32 @@ $('#add').on('click', function() { // wysyłanie danych z formularza
   const filmCarrier = $('#filmCarrier').val();
 
   const addFilm = {
-      name: filmName,
-      director: filmAuthor,
-      production: filmYear,
-      type: filmType,
-      kind: filmKind,
-      carrier: filmCarrier
+    name: filmName,
+    director: filmAuthor,
+    production: filmYear,
+    type: filmType,
+    kind: filmKind,
+    carrier: filmCarrier
   }
 
   firebase.database().ref('/films').push(addFilm); // dodajemy do bazy
-
   $('#exampleModal').modal('hide'); // zamykanie okna modalu
   $('form').trigger('reset'); // zresetowanie pól formularza
 })
 
-$('body').on('click', '#delete', function() { // "propagacja" dla elementów dynamicznych na stronie (ikonki pojawiają się na stronie dynamicznie, a nie statycznie)
+// "propagacja" dla elementów dynamicznych na stronie (ikonki pojawiają się na stronie dynamicznie, a nie statycznie)
+$('body').on('click', '#delete', function() {
   const self = $(this);
   const key = self.parent().data('key');
 
-  firebase.database()
-      .ref(`/films/${key}`)
-      .remove()
-      .then(function() {
-          //window.location.reload(); // 1 sposób - odświeżam całą stronę
-          self.parent().parent().remove() // 2 sposób - ręcznie usuwam z html'a
-      })
+  firebase
+    .database()
+    .ref(`/films/${key}`)
+    .remove()
+    .then(function() {
+      // window.location.reload(); // 1 sposób - odświeżam całą stronę
+      self.parent().parent().remove(); // 2 sposób - ręcznie usuwam z html'a
+    })
 })
 
 $('body').on('click', '#edit', function() {
@@ -105,18 +109,18 @@ $('body').on('click', '#accept', function() {
   currentCarrier.attr('contenteditable', false);
 
   const editedFilm = {
-      name: currentName.text(),
-      director: currentAuthor.text(),
-      production: currentYear.text(),
-      type: currentType.text(),
-      kind: currentKind.text(),
-      carrier: currentCarrier.text()
+    name: currentName.text(),
+    director: currentAuthor.text(),
+    production: currentYear.text(),
+    type: currentType.text(),
+    kind: currentKind.text(),
+    carrier: currentCarrier.text()
   }
 
   firebase
-      .database()
-      .ref(`/films/${key}`)
-      .set(editedFilm); // edytujemy w bazie danych
+    .database()
+    .ref(`/films/${key}`)
+    .set(editedFilm); // edytujemy w bazie danych
 
   self.addClass('invisible'); // ikonka do potwierdzenia edycji znika
   self.prev().removeClass('invisible'); // ikonka do edycji pojawia się
